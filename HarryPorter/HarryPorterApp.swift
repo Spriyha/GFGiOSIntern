2acb4f17333c60ab5ca8b6a47d7dcc7fce4e3da5
spriyha <mauryas00113@gmail.com>
2025-01-23 20:54:25 +0530

    Initial Commit

---
+++ HarryPorter/HarryPorterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPorterApp.swift
+//  HarryPorter
+//
+//  Created by Sagar Maurya on 23/01/25.
+//
+
+import SwiftUI
+
+@main
+struct HarryPorterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
